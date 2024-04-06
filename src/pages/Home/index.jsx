61ada1e7e44a4a37615a59e78810ca071bec26cd
Le Minh Chau <chau25241@gmail.com>
2024-04-06 10:17:05 +0700

    fix css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import styles from './home.module.css'
-import { Button, Inputs, Modal, ToolTip } from '@/components'
+import { Button, Inputs, Modal, ToolTip, Symtomps } from '@/components'
 import { Header, Footer, Navbar } from '@/layout'
 
 const index = () => {
-  return (
-    <div className={styles.root}>
-      <Header />
-      Home Pag
-      <br/>
-      {/* <Button/>
-      <Inputs/>
-      <Modal/>
-      <ToolTip/> */}
-    </div>
-  )
+	return (
+		<div className={styles.root}>
+			<Symtomps />
+		</div>
+	)
 }
 
 export default index
