73e5ceac4cd7edd8b9eb4ca33c0acbfacdea9820
DatNguyen60 <dat.nguyenthese80@gmail.com>
2024-04-06 13:55:47 +0700

    api

--- src/pages/ListDoctor/index.jsx
+++ src/pages/ListDoctor/index.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
+import { PiStudent } from 'react-icons/pi';
 import { Button } from 'antd';
 import { Avatar, List, Space } from 'antd';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const ListDoctor = () => {
-  return (
-    <div>
-      DatNguyen
-    </div>
-  )
-}
+  const [data, setdata] = useState(null);
 
-export default ListDoctor
+  useEffect(() => {
+      // Hàm fetchApiData sử dụng Axios để gửi yêu cầu GET đến API
+      const fetchApiData = async () => {
+          try {
+              const response = await axios.get("http://localhost:8080/doctor");
+              setdata(response.data);
+              console.log(response);
+          } catch (error) {
+          console.error('Error fetching data:', error);
+          }
+      };
+         // Gọi hàm fetchApiData khi component được mount
+         fetchApiData();
+        }, []); // [] đảm bảo useEffect chỉ chạy một lần khi component được mount
+        if (!data) return <></>;
+  return (
+   <div>
+      {data.map((doctor)=>(
+      
+          //  CODE 
+  
+      ))}
+      
+    </div>
+  );
+};
+
+export default ListDoctor;
